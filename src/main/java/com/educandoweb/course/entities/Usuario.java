7119e3283334dce55d02d0ffae4dc49bc0ebf453
Isabela Nogueira <isabela.nogueira@liferay.com>
2022-09-05 16:28:59 -0300

    H2 database, test profile, JPA

--- src/main/java/com/educandoweb/course/entities/Usuario.java
+++ src/main/java/com/educandoweb/course/entities/Usuario.java
@@ -3,27 +3,38 @@ package com.educandoweb.course.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class User  implements Serializable {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario  implements Serializable {
 	
 	private static final long serialVersionUID = 1L; // tem que colocar a versao
 	
 	
 	// usa-se o serializeble quando vc quer que os objetos (ex. User)
 	// possam ser transformados em cadeia de bytes pra que o objeto trafegue
-	//na rede, possa ser gravado em arquivo 
+	//na rede (ou possa ser gravado em arquivo)
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
-//	como eu estou usando framework, é obrigado colocar o construtor vazio
-	public User() {
+
+	//	como eu estou usando framework, é obrigado colocar o construtor vazio
+	public Usuario() {
 		
 	}
-//por conveniência
-	public User(Long id, String name, String email, String phone, String password) {
+
+	//por conveniência:
+	public Usuario(Long id, String name, String email, String phone, String password) {
 		super();
+	
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -74,7 +85,7 @@ public class User  implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		User other = (User) obj;
+		Usuario other = (Usuario) obj;
 		return Objects.equals(id, other.id);
 	}
 	
--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.educandoweb.course.entities.User;
+import com.educandoweb.course.entities.Usuario;
 
 @RestController //recurso web que eh implementado por um controlador REST
 @RequestMapping(value = "/users") //nome do recurso
@@ -15,8 +15,8 @@ public class UserResource {
 	//recurso web correspondente a entidade User.
 	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L,"Maria","maria@gmail.com","9999999","123456");
+	public ResponseEntity<Usuario> findAll() {
+		Usuario u = new Usuario(1L,"Maria","maria@gmail.com","9999999","123456");
 				return ResponseEntity.ok().body(u);
 	}
 	
